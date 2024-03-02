52784747630e42785dd7645f9c8618c1ef8c11f8
Achilles-3v <sh17vv@gmail.com>
2024-03-02 18:54:09 +0500

    new branch

---
+++ src/main/java/DocumentManagementSystem.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import javax.swing.text.Document;
+import java.util.List;
+
+public class DocumentManagementSystem {
+
+    void importFile(String path) {}
+
+    List<Document> contents() {
+        return null;
+    }
+}
