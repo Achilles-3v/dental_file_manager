a72c596ae8fc22a6ac0a186cf56e8b5d035190f2
Achilles-3v <sh17vv@gmail.com>
2024-03-03 23:10:20 +0500

    DocumentManagementSystem update

--- src/main/java/DocumentManagementSystem.java
+++ src/main/java/DocumentManagementSystem.java
@@ -1,9 +1,22 @@
 package main.java;
 
-import javax.swing.text.Document;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
+
+import static java.util.Collections.unmodifiableList;
 
 public class DocumentManagementSystem {
+
+    private final List<Document> documents = new ArrayList<>();
+    private final List<Document> documentsView = unmodifiableList(documents);
+    // tag::importer_lookup[]
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
+
+    public DocumentManagementSystem() {
+        extensionToImporter.put("letter", new LetterImporter());
+        extensionToImporter.put("report", new ReportImporter());
+        extensionToImporter.put("jpg", new ImageImporter());
+    }
 
     void importFile(String path) {}
 
