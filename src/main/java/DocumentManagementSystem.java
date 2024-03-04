245530401648c03f3edeb322fcbadcc64ae785f0
Achilles-3v <sh17vv@gmail.com>
2024-03-04 22:11:20 +0500

    DocumentManagementSystem update methods

--- src/main/java/DocumentManagementSystem.java
+++ src/main/java/DocumentManagementSystem.java
@@ -1,7 +1,10 @@
 package main.java;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.unmodifiableList;
 
@@ -9,7 +12,6 @@ public class DocumentManagementSystem {
 
     private final List<Document> documents = new ArrayList<>();
     private final List<Document> documentsView = unmodifiableList(documents);
-    // tag::importer_lookup[]
     private final Map<String, Importer> extensionToImporter = new HashMap<>();
 
     public DocumentManagementSystem() {
@@ -18,9 +20,41 @@ public class DocumentManagementSystem {
         extensionToImporter.put("jpg", new ImageImporter());
     }
 
-    void importFile(String path) {}
+    {
+        extensionToImporter.put("invoice", new InvoiceImporter());
+    }
+
+    void importFile(final String path) throws IOException {
+        final File file = new File(path);
+        if (!file.exists()) {
+            throw new FileNotFoundException(path);
+        }
+
+        final int separatorIndex = path.lastIndexOf('.');
+        if (separatorIndex != -1) {
+            if (separatorIndex == path.length()) {
+                throw new UnknownFileTypeException("No extension found For file: " + path);
+            }
+            final String extension = path.substring(separatorIndex + 1);
+            final Importer importer = extensionToImporter.get(extension);
+            if (importer == null) {
+                throw new UnknownFileTypeException("For file: " + path);
+            }
+
+            final Document document = importer.importFile(file);
+            documents.add(document);
+        } else {
+            throw new UnknownFileTypeException("No extension found For file: " + path);
+        }
+    }
 
     List<Document> contents() {
-        return null;
+        return documentsView;
+    }
+
+    public List<Document> search(final String query) {
+        return documents.stream()
+                .filter(Query.parse(query))
+                .collect(Collectors.toList());
     }
 }
